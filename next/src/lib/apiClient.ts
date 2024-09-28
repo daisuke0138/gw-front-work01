6420e3fe38c1374903dc08756abe36da42a897b4
04_W_daisuke_aramaki <daisuke_arsp@hotmail.com>
2024-09-28 15:57:01 +0900

    デプロイ２回目

--- next/src/lib/apiClient.ts
+++ next/src/lib/apiClient.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-// const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASEURL;
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASEURL;
 const apiClient = axios.create({
-    baseURL: "http://localhost:3001/api",
-    // baseURL: apiBaseUrl,
-
-
+    // baseURL: "http://localhost:3001/api",
+    baseURL: apiBaseUrl,
     headers: {
         "Content-Type": "application/json",
     },
@@ -15,10 +13,5 @@ export const fetchData = async (endpoint: string) => {
     const response = await apiClient.get(`/api/${endpoint}`);
     return response.data;
 };
-
-// baseURL: process.env.NODE_ENV === 'development'
-//     ? 'http://localhost:3001/api'
-//     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-
 
 export default apiClient;
